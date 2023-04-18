099019e9102b8209443585f9db9381ae49465a7e
floodlit-torte <arkeyalibi171007@gmail.com>
2023-04-18 17:53:39 +0600

    Enemy AI

---
+++ Assets/Script/EnemyAI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float chaseRange = 5f;
+
+    private NavMeshAgent _agent;
+    private float _distanceToTarget;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if(_distanceToTarget < chaseRange)
+        {
+            _agent.SetDestination(target.position);
+        }    
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+#endif
+}
